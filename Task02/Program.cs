4e0b6192a64c8ad1db5f31b1107e7bf595695873
Anna <kochurovaav@gmail.com>
2023-02-01 21:20:10 +0200

    Task02 is added

---
+++ Task02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Принимает два числа, выводит кратное ли второе число пепвому;
+// Если нет, то выводит остаток.
+
+Console.WriteLine("Enter number one :");
+string strnumberA = Console.ReadLine();
+int numberA = Convert.ToInt32(strnumberA);
+
+
+Console.WriteLine("Enter numbertwo:");
+string strnumberB = Console.ReadLine();
+int numberB = Convert.ToInt32(strnumberB);
+
+double result = numberA%numberB;
+
+if (numberA/numberB == 0)
+{
+     Console.WriteLine("Multiple");
+}
+
+else
+ {
+    Console.WriteLine($"Not multiple, {result}");
+ }
