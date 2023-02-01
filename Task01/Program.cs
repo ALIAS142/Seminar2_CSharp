dce8c89ce2a7273cb712f70ee7bfe47731c3ae57
Anna <kochurovaav@gmail.com>
2023-02-01 19:28:37 +0200

    Changesin Task01 is added

---
+++ Task01/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа выводит рандомное трехзначное число и удаляет вторую цифру числа.
+
+int number = new Random().Next (100, 1000);
+
+
+
+Console.WriteLine($"number = {number}");
+
+Console.Write($"{number/100} {number%10}");
+
