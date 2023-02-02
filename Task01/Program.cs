e1be6aeedcf74806e57f8afb7e53d04b2ccbc4e1
Anna <kochurovaav@gmail.com>
2023-02-02 13:46:43 +0200

    Changes are done

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -6,5 +6,7 @@ int number = new Random().Next (100, 1000);
 
 Console.WriteLine($"number = {number}");
 
-Console.Write($"{number/100} {number%10}");
+Console.WriteLine($"{number/100}{number%10}");
 
+int result = (number/100) * 10 + (number%10);
+Console.Write(result);
