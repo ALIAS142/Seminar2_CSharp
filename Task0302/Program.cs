21999dec135cff832155b95ccec01947b11d5ac3
Anna <kochurovaav@gmail.com>
2023-02-06 17:32:47 +0200

    Task0302 is addded

---
+++ Task0302/Program.cs
@@ -0,0 +1,23 @@
+﻿
+var random = new Random();
+
+int randomNum = 33;//random.Next(10, 100);
+
+System.Console.WriteLine(randomNum);
+
+int num1 = randomNum / 10;
+
+int num2 = randomNum % 10;
+
+if (num1 > num2)
+{
+    System.Console.WriteLine(num1);
+}
+else if (num1 < num2)
+{
+    System.Console.WriteLine(num2);
+}
+else
+{
+    System.Console.WriteLine($"Они рaвны оба рaвны {num1}");
+}
